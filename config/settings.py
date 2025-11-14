e9ad062f1fec1ff2643d4fe36c97deaaedf9a962
= <=>
2025-11-14 12:35:35 +0600

    debug false

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^)0%hdy$=bg#%oa%fo23@2&&=l10q9clo$k0oy05fw&no8aenn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "sn-02ce.onrender.com",
